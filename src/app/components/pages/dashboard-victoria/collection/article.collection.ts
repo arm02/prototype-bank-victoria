03847f35edf3646b2dcc301b32800844c2118272
kevinstategami <36149962+kevinstategami@users.noreply.github.com>
2025-11-11 02:36:27 +0700

    feat: adjustment collection article

--- src/app/components/pages/dashboard-victoria/collection/article.collection.ts
+++ src/app/components/pages/dashboard-victoria/collection/article.collection.ts
@@ -16,25 +16,9 @@ export const ANOTHER_ARTICLE = [
         title: 'Mengapa Web Development Penting di Era Digital?'
     },
     {
-        img: './assets/bank-victoria/images/illust-related-article.png',
-        title: 'Mengapa Web Development Penting di Era Digital?'
-    },
-    {
-        img: './assets/bank-victoria/images/illust-related-article.png',
-        title: 'Mengapa Web Development Penting di Era Digital?'
-    },
-    {
         img: './assets/bank-victoria/images/illust-side-article.png',
         title: 'Mengapa Web Development Penting di Era Digital?'
     },
-    {
-        img: './assets/bank-victoria/images/illust-related-article-2.png',
-        title: 'Mengapa Web Development Penting di Era Digital?'
-    },
-    {
-        img: './assets/bank-victoria/images/illust-side-article.png',
-        title: 'Mengapa Web Development Penting di Era Digital?'
-    },    
 ]
 
 export const RELATED_ARTICLE = [
@@ -48,18 +32,6 @@ export const RELATED_ARTICLE = [
     },
     {
         img: './assets/bank-victoria/images/illust-side-article.png',
-        title: 'Mengapa Web Development Penting di Era Digital?'
-    },
-    {
-        img: './assets/bank-victoria/images/illust-related-article-2.png',
-        title: 'Mengapa Web Development Penting di Era Digital?'
-    },
-    {
-        img: './assets/bank-victoria/images/illust-related-article.png',
-        title: 'Mengapa Web Development Penting di Era Digital?'
-    },
-    {
-        img: './assets/bank-victoria/images/illust-related-article.png',
         title: 'Mengapa Web Development Penting di Era Digital?'
     },
 ]
