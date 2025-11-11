f11cfb32333e89994c7eb2ff442b1fe64250b724
Adrian <arm.adrian02@gmail.com>
2025-11-11 17:01:55 +0700

    feat: certification & programs detail

--- src/app/components/pages/bank-victoria/certification/certification.component.ts
+++ src/app/components/pages/bank-victoria/certification/certification.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { SERTIFIKAT_LIST } from '../program/program.collection';
 
 @Component({
   selector: 'app-certification',
   templateUrl: './certification.component.html',
   styleUrls: ['./certification.component.scss'],
 })
-export class CertificationComponent {}
+export class CertificationComponent {
+  coursesList = structuredClone(SERTIFIKAT_LIST);
+}
